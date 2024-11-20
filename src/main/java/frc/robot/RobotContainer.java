6d6db134b5c256150e045249e4a9bda75d531e2c
Duel M <dueromoods@gmail.com>
2024-11-19 19:53:23 -0500

    Refactored creation of drivetrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,26 +4,13 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Subsystems.Swerve.*;
 
 public class RobotContainer {
-  Drivetrain drivetrain = new Drivetrain(3, 3, new SwerveModule[] {
-      new SwerveModule(new CANSparkMax(3, MotorType.kBrushless), new CANSparkMax(4, MotorType.kBrushless),
-          0.8288317, "Front left"), // front left, 3, 4
-      new SwerveModule(new CANSparkMax(1, MotorType.kBrushless), new CANSparkMax(2, MotorType.kBrushless),
-          0.9547673, "Front right"), // front right 1, 2
-      new SwerveModule(new CANSparkMax(5, MotorType.kBrushless), new CANSparkMax(6, MotorType.kBrushless),
-          0.7699317, "Back left"), // back left 5, 6
-      new SwerveModule(new CANSparkMax(7, MotorType.kBrushless), new CANSparkMax(8, MotorType.kBrushless),
-          0.0163250, "Back right") // back right 7, 8
-      // drive motors are odd, turning motors are even
-  });
+  Drivetrain drivetrain = new Drivetrain();
 
   public RobotContainer() {
     configureBindings();
